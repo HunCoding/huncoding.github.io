5be9f3c1da546ea784a1b202ca4549ba6f63a6df
HunnTeRUS <otavio201378@gmail.com>
2025-09-19 17:37:23 -0300

    Remove all newsletter functionality
    - Delete all newsletter-related files and configurations
    - Remove newsletter references from layouts and includes
    - Clean up JavaScript imports and components
    - Remove Mailchimp integration completely
    - Rebuild JavaScript without newsletter components

--- _javascript/home.js
+++ _javascript/home.js
@@ -1,11 +1,8 @@
 import { basic, initSidebar, initTopbar } from './modules/layouts';
-import { initLocaleDatetime, loadImg, Newsletter } from './modules/components';
+import { initLocaleDatetime, loadImg } from './modules/components';
 
 loadImg();
 initLocaleDatetime();
 initSidebar();
 initTopbar();
 basic();
-
-// Initialize newsletter
-new Newsletter();
